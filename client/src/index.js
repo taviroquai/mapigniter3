c25ff62dc79eefec9756318137920857265d3cbc
Marco Afonso <mafonso333@gmail.com>
2018-03-26 11:45:16 +0100

    Move initial state to config.json

--- client/src/index.js
+++ client/src/index.js
@@ -8,18 +8,6 @@ import registerServiceWorker from './registerServiceWorker';
 import state from './config.json';
 
 // Initial state
-state['login'] = {}
-state['recover'] = {}
-state['reset'] = {token: ''}
-state['profile'] = {user: false}
-state['home'] = {maps: []}
-state['dashboard'] ={data1: [], loading: true}
-state['layertype'] = {form: {}, sortc: "id", sortd: "ascending"}
-state['projection'] = {form: {}, sortc: "id", sortd: "ascending"}
-state['layer'] = {form: {}, sortc: "id", sortd: "ascending"}
-state['map'] = {form: {layers: []}, sortc: "id", sortd: "ascending"}
-state['maplayer'] = {form: {layers: []}, sortc: "id", sortd: "ascending"}
-state['mapdisplay'] = {current: null}
 Store.init(state, true);
 
 // Start app
